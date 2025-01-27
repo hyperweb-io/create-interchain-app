3d9b7c4f6c1393f50eea0d50889d591e325a3b72
Zetazzz <zetazz@163.com>
2025-01-27 11:18:01 +0800

    rename

--- packages/create-interchain-app/src/cli.ts
+++ packages/create-interchain-app/src/cli.ts
@@ -1,7 +1,7 @@
-import { CCA_URL } from "./constants";
+import { CIA_URL } from "./constants";
 import { createGitApp } from "./git-cia-template";
 export const cli = async (argv, version) => {
-  const repo = argv.repo ?? CCA_URL;
+  const repo = argv.repo ?? CIA_URL;
   const createCosmosApp = createGitApp(repo, version);
   await createCosmosApp(argv);
 };
