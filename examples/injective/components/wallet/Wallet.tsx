747c88328af25d5a0e6622b56d453da1573fb951
Zetazzz <zetazz@163.com>
2025-02-21 19:28:53 +0800

    bundle test

--- examples/injective/components/wallet/Wallet.tsx
+++ examples/injective/components/wallet/Wallet.tsx
@@ -1,27 +1,45 @@
-import { Box, Stack, ClipboardCopyText, useColorModeValue } from '@interchain-ui/react';
-import { WalletState } from '@interchain-kit/core'
+import {
+  Box,
+  Stack,
+  ClipboardCopyText,
+  useColorModeValue,
+} from '@interchain-ui/react';
+import { WalletState } from '@interchain-kit/core';
 import { useChain } from '@interchain-kit/react';
 import { getChainLogo } from '@/utils';
-import { defaultChainName } from "@/config";
+import { defaultChainName } from '@/config';
 import { User } from './User';
 import { Chain } from './Chain';
 import { Button } from './Connect';
 import { Warning } from './Warning';
 
 export function Wallet() {
-  const { chain, status, wallet, username, address, message, connect, openView } = useChain(defaultChainName);
+  const {
+    chain,
+    status,
+    wallet,
+    username,
+    address,
+    message,
+    connect,
+    openView,
+  } = useChain(defaultChainName);
 
   const ConnectButton = {
     [WalletState.Connected]: <Button.Connected onClick={openView} />,
     [WalletState.Connecting]: <Button.Connecting />,
     [WalletState.Disconnected]: <Button.Disconnected onClick={openView} />,
-    [WalletState.Reject]: <Button.Rejected onClick={openView} />
+    [WalletState.Rejected]: <Button.Rejected onClick={openView} />,
+    [WalletState.NotExist]: <Button.Connect onClick={openView} />,
   }[status] || <Button.Connect onClick={openView} />;
 
   return (
     <Box py="$16">
       <Stack attributes={{ mb: '$12', justifyContent: 'center' }}>
-        <Chain name={chain.prettyName || 'prettyName unknown'} logo={getChainLogo(chain.chainName)!} />
+        <Chain
+          name={chain.prettyName || 'prettyName unknown'}
+          logo={getChainLogo(chain.chainName)!}
+        />
       </Stack>
       <Stack
         direction="vertical"
@@ -36,11 +54,13 @@ export function Wallet() {
           boxShadow: useColorModeValue(
             '0 0 2px #dfdfdf, 0 0 6px -2px #d3d3d3',
             '0 0 2px #363636, 0 0 8px -2px #4f4f4f'
-          )
+          ),
         }}
       >
         {username ? <User name={username} /> : null}
-        {address ? <ClipboardCopyText text={address} truncate="middle" /> : null}
+        {address ? (
+          <ClipboardCopyText text={address} truncate="middle" />
+        ) : null}
         <Box
           my="$8"
           flex="1"
@@ -49,14 +69,15 @@ export function Wallet() {
           height="$16"
           overflow="hidden"
           justifyContent="center"
-          px={{ mobile: '$8', tablet: '$10' }}>
+          px={{ mobile: '$8', tablet: '$10' }}
+        >
           {ConnectButton}
         </Box>
 
-        {message && [WalletState.Reject].includes(status)
-          ? <Warning text={`${wallet?.option?.prettyName}: ${message}`} />
-          : null}
+        {message && [WalletState.Rejected].includes(status) ? (
+          <Warning text={`${wallet?.info?.name}: ${message}`} />
+        ) : null}
       </Stack>
     </Box>
-  )
+  );
 }
