eadc01741fa3d32fdfefb0ab8a4486d187845c20
Gefei Hou <noroldburden@gmail.com>
2025-04-14 14:47:37 +0800

    Added protod command from telescope download

--- packages/create-interchain-app/src/cli.ts
+++ packages/create-interchain-app/src/cli.ts
@@ -1,7 +1,13 @@
 import { CIA_URL } from "./constants";
 import { createGitApp } from "./git-cia-template";
+import protodCommand from '@cosmology/telescope/main/commands/download'
 export const cli = async (argv, version) => {
+  if (argv._[0] === 'protod') {
+    console.log('got protod')
+    await protodCommand(argv);
+  } else {
   const repo = argv.repo ?? CIA_URL;
   const createCosmosApp = createGitApp(repo, version);
   await createCosmosApp(argv);
+  }
 };
