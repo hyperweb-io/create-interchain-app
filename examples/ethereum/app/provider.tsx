ec011ae4c46fe6c84e89b05004dbf99881d11f73
Eason <sdqede@gmail.com>
2025-04-30 21:11:47 +1200

    add assets

--- examples/ethereum/app/provider.tsx
+++ examples/ethereum/app/provider.tsx
@@ -11,6 +11,7 @@ const _wallets = [
   metaMaskWallet,
 ];
 
+// reference: https://github.com/hyperweb-io/interchain-kit/blob/main/examples/react/src/main.tsx#L86
 export const HOLESKY_TESTNET = {
   chainId: "0x4268", // 17000 | 0x4268
   chainName: "Holesky testnet",
@@ -35,12 +36,56 @@ export const BSC_TESTNET = {
   blockExplorerUrls: ["https://testnet.bscscan.com"],
 };
 
+const assets = [
+  {
+    "description": "",
+    "extendedDescription": "",
+    "denomUnits": [
+      {
+        "denom": "wei",
+        "exponent": 0
+      },
+      {
+        "denom": "gwei",
+        "exponent": 9
+      },
+      {
+        "denom": "eth",
+        "exponent": 18,
+        "aliases": [
+          "ether"
+        ]
+      }
+    ],
+    "typeAsset": "evm-base",
+    "base": "wei",
+    "name": "Holesky ETH",
+    "display": "eth",
+    "symbol": "ETH",
+    "logoURIs": {
+      "png": "https://raw.githubusercontent.com/cosmos/chain-registry/master/_non-cosmos/ethereum/images/eth-white.png",
+      "svg": "https://raw.githubusercontent.com/cosmos/chain-registry/master/_non-cosmos/ethereum/images/eth-white.svg"
+    },
+    "coingeckoId": "ethereum",
+    "images": [
+      {
+        "png": "https://raw.githubusercontent.com/cosmos/chain-registry/master/_non-cosmos/ethereum/images/eth-white.png",
+        "svg": "https://raw.githubusercontent.com/cosmos/chain-registry/master/_non-cosmos/ethereum/images/eth-white.svg",
+        "theme": {
+          "primaryColorHex": "#303030"
+        }
+      }
+    ]
+  }
+]
+
 export default function Provider({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   console.log('chain', chain)
+  console.log('assetList', assetList)
   return (
     <ThemeProvider themeMode='light'>
       <ChainProvider
@@ -52,7 +97,10 @@ export default function Provider({
         ]}
         // @ts-ignore
         wallets={_wallets}
-        assetLists={[assetList]}
+        assetLists={[{
+          ...assetList,
+          assets: [...assetList.assets, ...assets]
+        }]}
         signerOptions={{}}
       >
         {children}
